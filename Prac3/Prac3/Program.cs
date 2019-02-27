28297b745a2a768a8186bd4f0a8a60beeb43cc44
Georgina Bianca Fiorentinos <geofio@live.co.za>
2019-02-27 16:23:17 +0200

    1 and 2 finished

---
+++ Prac3/Prac3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericSet<string> s = new GenericSet<string>();
+
+            s.Add("cat");
+            s.Add("dog");
+            Console.WriteLine(s);
+            s.Add("cat");
+            Console.WriteLine(s);
+            s.Remove(1);
+            Console.WriteLine(s);
+            Console.WriteLine(s.Length());
+            Console.Read();
+           
+        }
+    }
+}
